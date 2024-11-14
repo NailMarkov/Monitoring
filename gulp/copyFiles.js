ab6500c7c97fea9941dd558c96e3cc59158f0e28
Nail Markov <markov_nail@mail.ru>
2024-11-14 14:13:52 +0300

    Добавляет шаблон проекта

---
+++ gulp/copyFiles.js
@@ -0,0 +1,26 @@
+import gulp from 'gulp';
+
+// Import from files
+
+import {pathBuild, pathSrc} from './conf.js';
+
+const copySvg = () =>
+  gulp.src('source/img/**/*.svg', {base: `${pathSrc.src}`})
+      .pipe(gulp.dest(`${pathBuild.build}`));
+
+const copyImages = () =>
+  gulp.src('source/img/**/*.{png,jpg,webp}', {base: `${pathSrc.src}`, encoding: false})
+      .pipe(gulp.dest(`${pathBuild.build}`));
+
+const copy = () =>
+  gulp.src([
+    `${pathSrc.fonts}/**`,
+    `${pathSrc.img}/**`,
+    `${pathSrc.js}/**`,
+  ], {
+    base: `${pathSrc.src}`,
+    encoding: false,
+  })
+      .pipe(gulp.dest(`${pathBuild.build}`));
+
+export {copy, copyImages, copySvg};
